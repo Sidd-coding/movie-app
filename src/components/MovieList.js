fbc953abe01aacc34c665147f213c05d9e2ebafc
Sidd-coding <rahulsrivastavasidd@gmail.com>
2021-06-03 22:38:24 +0530

    movielist

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,7 @@ const MovieList = (props) => {
 
 	return (
 		<>
-			{(props.movies.length > 0) ? props.movies.map((movie, index) => (
+			{(props.movies) ? props.movies.map((movie, index) => (
 				<div className='image-container d-flex justify-content-start col-md-3'
                 key={index}>
                 	<img src={movie.Poster} alt={movie.Title}></img>
